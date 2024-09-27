ddb5f1da5161f63df69e3a34ada1f0a166256a39
Manish Singh <msingh30497@gmail.com>
2024-09-27 17:54:50 +0530

    Second Commit

---
+++ src/Parctice/HelloPrintForGitCommit.java
@@ -0,0 +1,7 @@
+package Parctice;
+
+public class HelloPrintForGitCommit {
+public static void main(String[] args) {
+	System.out.println("Hello");
+}
+}
--- src/Parctice/RemovingDuplicateWithoutCollection.java
+++ src/Parctice/RemovingDuplicateWithoutCollection.java
@@ -22,5 +22,6 @@ public static void main(String[] args) {
 	for(int elem:unique) {
 		System.out.println(elem);
 	}
+	System.out.println("Th");
 }
 }
